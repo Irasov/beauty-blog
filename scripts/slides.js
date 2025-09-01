1c08afae1f39f3523b465be401cd36aff5fcd222
Irasov <srnan@yandex.ru>
2025-09-01 11:33:45 +0500

    fix: height and padding image for slider

--- scripts/slides.js
+++ scripts/slides.js
@@ -1,26 +1,29 @@
 let slides = [
-    {
-        "img": "img/skincare.webp",
-        "alt": "scincare",
-        "cat": "FEATURED",
-        "title": "10 Must-Have Skincare Products for Radiant Skin",
-        "subtitle": "Explore the essential skincare products that can transform your daily routine. From hydrating serums to powerful antioxidants, discover the secrets to achieving radiant and healthy skin.",
-        "link": "#"
-    },
-    {
-        "img": "img/skincare2.webp",
-        "alt": "scincare",
-        "cat": "FEATURED",
-        "title": "15 Must-Have Skincare Products for Radiant Skin",
-        "subtitle": "Explore the essential skincare products that can transform your daily routine. From hydrating serums to powerful antioxidants, discover the secrets to achieving radiant and healthy skin.",
-        "link": "#"
-    },
-    {
-        "img": "img/skincare3.webp",
-        "alt": "scincare",
-        "cat": "FEATURED",
-        "title": "20 Must-Have Skincare Products for Radiant Skin",
-        "subtitle": "Explore the essential skincare products that can transform your daily routine. From hydrating serums to powerful antioxidants, discover the secrets to achieving radiant and healthy skin.",
-        "link": "#"
-    },
-]
+  {
+    img: 'img/skincare.webp',
+    alt: 'scincare',
+    cat: 'FEATURED',
+    title: '10 Must-Have Skincare Products for Radiant Skin',
+    subtitle:
+      'Explore the essential skincare products that can transform your daily routine. From hydrating serums to powerful antioxidants, discover the secrets to achieving radiant and healthy skin.',
+    link: '#',
+  },
+  {
+    img: 'img/skincare2.jpg',
+    alt: 'scincare',
+    cat: 'FEATURED',
+    title: '15 Must-Have Skincare Products for Radiant Skin',
+    subtitle:
+      'Explore the essential skincare products that can transform your daily routine. From hydrating serums to powerful antioxidants, discover the secrets to achieving radiant and healthy skin.',
+    link: '#',
+  },
+  {
+    img: 'img/skincare3.jpg',
+    alt: 'scincare',
+    cat: 'FEATURED',
+    title: '20 Must-Have Skincare Products for Radiant Skin',
+    subtitle:
+      'Explore the essential skincare products that can transform your daily routine. From hydrating serums to powerful antioxidants, discover the secrets to achieving radiant and healthy skin.',
+    link: '#',
+  },
+];
